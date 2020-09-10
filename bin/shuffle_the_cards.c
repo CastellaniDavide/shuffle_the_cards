dfa7e842e261d2772cfdcf3d9f3dbae75cad15b2
DavideC03 <davidecastellani@castellanidavide.it>
2020-09-10 10:02:09 +0200

    v1.0

--- bin/shuffle_the_cards.c
+++ bin/shuffle_the_cards.c
@@ -14,9 +14,63 @@
  *
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-	// printf() displays the string inside quotation
-	printf("shuffle_the_cards");
+#define CARDS 52
+#define FACES 13
+
+// Struct
+typedef struct
+{
+	const char *face;
+	const char *suit;
+} Card;
+
+// Functions
+void fillDeck(Card *const wDeck, const char *wFace[], const char *wSuit[]);
+void shuffle(Card *const wDeck);
+void deal(const Card *const wDeck);
+
+int main()
+{
+	Card deck[CARDS];
+	srand(time(NULL));
+
+	const char *face[] = {"Ace", "Deuce", "Tree", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
+	const char *suit[] = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+	fillDeck(deck, face, suit);
+	shuffle(deck);
+	deal(deck);
+
 	return 0;
+}
+
+void fillDeck(Card *const wDeck, const char *wFace[], const char *wSuit[])
+{
+	for(size_t i = 0; i < CARDS; i++)
+	{
+		wDeck[i].face = wFace[i % FACES];
+		wDeck[i].suit = wSuit[i / FACES];
+	}
+}
+
+void shuffle(Card *const wDeck)
+{
+	for(size_t i = 0; i < CARDS; i++)
+	{
+		int j = rand() % CARDS;
+		Card temp = wDeck[i];
+		wDeck[i] = wDeck[j];
+		wDeck[j] = temp;
+	}
+}
+
+void deal(const Card *const wDeck)
+{
+	for(size_t i = 0; i < CARDS; i++)
+	{
+		printf("%2ld) %5s of %-8s\n", i+1, wDeck[i].face, wDeck[i].suit);
+	}
 }
