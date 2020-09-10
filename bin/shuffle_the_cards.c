8ad2ed3238ee987127596d2af57340efcc2661aa
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-10 09:28:11 +0200

    Created bin/template.c

---
+++ bin/shuffle_the_cards.c
@@ -0,0 +1,22 @@
+/**
+ * @file shuffle_the_cards.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief This is a c code to shuffle the cards "vitually"
+ *
+ * @ingroup shuffle_the_cards
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("shuffle_the_cards");
+	return 0;
+}
